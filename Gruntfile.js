9375d434cbd6a6e01391770bbfc90b0f7c816391
Matt Fawcett <fawcett@viddler.com>
2014-10-03 12:15:07 +0100

    Add watch tasks to update js when coffeescript changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
     coffee: {
@@ -8,7 +9,16 @@ module.exports = function(grunt) {
           'js/upload.js': ['src/uploader.js.coffee']
         }
       }
-    }
+    },
+    watch: {
+      scripts: {
+        files: ['**/*.js.coffee'],
+        tasks: ['coffee'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
   });
 
 
